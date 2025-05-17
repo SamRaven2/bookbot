bf41e4d4beb1211acb562e0bd489a80df8e21634
Bats <samraven2@gmail.com>
2025-05-16 23:58:36 -0400

    Remainder of Lessons for Project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words
+from stats import *
+import sys
 
 def get_book_text(filepath):
     try:
@@ -8,10 +9,32 @@ def get_book_text(filepath):
     except Exception:
         print("Filepath is invalid.")
 
+def print_report(file_path, word_count, sorted_letter_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for set in sorted_letter_list:
+        if set["char"].isalpha():
+            print(f"{set["char"]}: {set["num"]}")
+    print("============= END ===============")
+    return
+
 def main():
-    book_filepath = "./books/frankenstein.txt"
-    wordcount = count_words(get_book_text(book_filepath))
-    print(f"{wordcount} words found in the document")
+    arguments = sys.argv
+
+    if len(arguments) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_filepath = arguments[1]
+    book_text = get_book_text(book_filepath)
+    wordcount = count_words(book_text)
+    sorted_lettercounts = sort_dict(count_letters(book_text))
+
+    print_report(book_filepath, wordcount, sorted_lettercounts)
+    
     return
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def count_words(input_string):
     words_list = input_string.split()
     return len(words_list)
+
+def count_letters(input_string):
+    letter_counts = {}
+    for char in input_string:
+        if char.lower() in letter_counts:
+            letter_counts[char.lower()] += 1
+        else:
+            letter_counts[char.lower()] = 1
+    return letter_counts
+
+def sort_on(unsorted_dict):
+    return unsorted_dict["num"]
+
+def sort_dict(unsorted_dict):
+    multi_dict_list = []
+    for item in unsorted_dict:
+        multi_dict_list.append({"char": item, "num": unsorted_dict[item]})
+    multi_dict_list.sort(reverse=True, key=sort_on)
+    return multi_dict_list
